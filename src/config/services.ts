b6f370fd5c6c47f2e66d24f770247b357300a74a
saufth <saufth@gmail.com>
2024-02-19 20:14:27 -0600

    Update service image source

--- src/config/services.ts
+++ src/config/services.ts
@@ -39,7 +39,7 @@ export const services: Item[] = [
     description: 'Lorem ipsun dolor sit amet, consectetur adipiscing elit. Nulla quam velit ipsun dolor sit amet, consectetur adipiscing elit. Nulla quam velit ipsun dolor sit amet.',
     slug: '/desarrolladora',
     image: {
-      src: '/images/service3.png',
+      src: '/images/service4.png',
       alt: 'Lorem ipsun dolor sit amet, consectetur adipiscing elit. Nulla quam velit ipsun dolor sit amet, consectetur adipiscing elit. Nulla quam velit ipsun dolor sit amet',
       width: 1280,
       height: 800
