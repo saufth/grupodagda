3876e1cf0545bc83ca2ab725765d99b59560963e
saufth <saufth@gmail.com>
2024-03-05 16:32:39 -0600

    Add dynamic import for BackgroundSmoke component

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
 import SiteFooter from '@/components/layouts/site-footer'
 import SiteHeader from '@/components/layouts/site-header'
-import BackgroundSmoke from '@/components/background-smoke'
+import { Icons } from '@/components/icons'
+
+const BackgroundSmoke = dynamic(() => import('@/components/background-smoke'), {
+  ssr: false
+})
+
+const Loading = () => (
+  <div className='absolut inset-0 bg-black grid place-content-center'>
+    <Icons.Logomark className='w-16 h-16 m-auto fill-accent' />
+  </div>
+)
 
 export default async function LobbyLayout ({ children }: React.PropsWithChildren) {
   return (
-    <BackgroundSmoke>
-      <SiteHeader />
-      <main>
-        {children}
-      </main>
-      <SiteFooter />
-    </BackgroundSmoke>
+    <React.Suspense fallback={<Loading />}>
+      <BackgroundSmoke>
+        <SiteHeader />
+        <main>
+          {children}
+        </main>
+        <SiteFooter />
+      </BackgroundSmoke>
+    </React.Suspense>
   )
 }
